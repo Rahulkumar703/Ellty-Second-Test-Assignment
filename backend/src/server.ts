027c9ee052a06ad422099faf9efd4b14079c0bf0
Rahul <rahulkumar703@outlook.com>
2025-10-16 10:54:38 +0530

    removed docker and added vercel configration in backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,11 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { apiLimiter } from "./middlewares/rate-limiter.middleware";
+import mongoose from "mongoose";
+import { Request, Response } from "express";
+import { authRoutes, operationRoutes, startingNumberRoutes } from "./routes";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -22,5 +27,34 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 app.use(apiLimiter);
+
+// Health check route
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Welcome to the API" });
+});
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/starting-number", startingNumberRoutes);
+app.use("/api/operation", operationRoutes);
+
+const PORT = process.env.APP_PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI!)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error("Database error:", err));
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log(
+      `Database status: ${
+        mongoose.connection.readyState === 1 ? "Connected" : "Connecting..."
+      }`
+    );
+  });
+}
 
 export default app;
